67e6f38f001fbb44feb728c7922327b700e0172c
Noam <noamru22@gmail.com>
2022-11-24 11:55:24 +0100

    styled homepage

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -8,11 +8,11 @@ const Header: React.FC = () => {
 
   return (
     <header className="mb-5">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+      <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center max-w-screen-xl">
           <Link href="/" className="flex items-center left-0">
-            <span className="text-xl font-semibold whitespace-nowrap">
-              HOST ON CHAIN
+            <span className="subtitle text-[#9B869C] text-lg font-bold whitespace-nowrap">
+              host on chain
             </span>
           </Link>
           <div className="flex items-center">{address && <Logout />}</div>
