9c65602461ea53720fbc920e815dd306e986e817
Noam <noamru22@gmail.com>
2022-11-24 12:55:50 +0100

    styled room

--- frontend/components/Room/Room.tsx
+++ frontend/components/Room/Room.tsx
@@ -25,37 +25,44 @@ const Room: React.FC = () => {
 
   const room: Room = {
     id: 1,
-    name: "Room 1",
-    description: "Room 1 description",
+    name: "Cosy and warm room",
+    description:
+      "A room in a quite area. Provided with private bathroom and kitchen.",
     price: 100,
     balance: 20,
     image: "/images/room.jpg",
-    address: "Berlin, Gerrmany",
+    address: "Berlin, Germany",
   };
   return (
     <div className="flex justify-center">
-      <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+      <div className="block rounded-xl shadow-lg bg-white max-w-sm text-center mt-14 px-10">
         <div className="flex flex-col items-center p-3">
+          <h5 className="text-gray-900 text-3xl font-bold subtitle m-5">
+            {room.name}
+          </h5>
           <img className="rounded-full" alt="presentation" src={room.image} />
-          <p className="m-3 border-b border-gray-300">{room.address}</p>
+          <p className="text-gray-700 text-base subtitle mt-6">
+            {room.description}
+          </p>
+          <p className="m-3 border-b subtitle border-gray-300">
+            {room.address}
+          </p>
         </div>
-        <div className="p-5">
-          <h5 className="text-gray-900 text-xl font-medium">{room.name}</h5>
-          <p className="text-gray-700 text-base">{room.description}</p>
-          <div className="p-5">
-            <p className="italic text-center rounded-md">
-              Targetted Amount : {room.price}
+        <div>
+          <div className="p-1">
+            <p className="subtitle text-center rounded-md font-medium">
+              Targeted Amount : €{room.price}
             </p>
-            <p className="italic text-center rounded-md">
-              Amount collected : {room.balance}
+            <p className="subtitle text-center rounded-md font-medium">
+              Amount collected : €{room.balance}
             </p>
           </div>
 
-          <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
+          <div className="md:flex md:items-center m-6">
+            <div className="md:w-1/3 subtitle">
               <Label label={"Donation in €"} />
             </div>
-            <div className="md:w-2/3">
+            <div className="md:w-2/3 subtitle">
               <Input
                 onChange={(e) => {
                   setDonation(parseInt(e.target.value));
@@ -63,8 +70,9 @@ const Room: React.FC = () => {
               />
             </div>
           </div>
-
-          <Button title={"Donate"} onClick={handleDonate} />
+          <div className="mb-6">
+            <Button title={"Donate"} onClick={handleDonate} />
+          </div>
         </div>
       </div>
     </div>
